012b61802df4181d8fbdf322f2fcef96975f2d73
Claire Royer <claireroyer57@gmail.com>
2024-06-15 22:30:21 +0200

    Add logic for Stars component

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -2,10 +2,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const Stars = ({ rating }) => {
+  const coloredStarsArr = [...Array(parseInt(rating)).keys()];
+  const greyStarsArr = [...Array(5 - coloredStarsArr.length).keys()];
+
   return (
-    <div>
-      <FontAwesomeIcon icon={faStar} />
-    </div>
+    <>
+      {coloredStarsArr.map(r => <FontAwesomeIcon key={r} icon={faStar} className="rating-up" />)}
+      {greyStarsArr.map(r => <FontAwesomeIcon key={r} icon={faStar} className="rating-down" />)}
+    </>
   );
 };
 
